dfdc65b4351cc8ee97e3f19985983ebc7a9f3dc6
Mahmoud M. Akasha <144206040+maakakasha@users.noreply.github.com>
2024-05-08 14:03:19 +0400

    dummy message

---
+++ provider_abstraction.dart
@@ -0,0 +1,17 @@
+class DataOfInterest { // with change notifier 
+  int? _count;
+
+  void increment () {
+    _count++;
+    // call the changeNotifier method 
+  }
+
+  // The calss is ready!
+  
+  // Usage:
+  // wrap the main app widget with provider
+  // add the list of different providers to the application
+  // Use the consumer widget to isolate the interest
+  // use the provider class' variable as if it was a normal 
+  // variable.
+}
